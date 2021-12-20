6751964037426a2c925cf14628a35272ce6def75
ituwei <61059082+Isack-321@users.noreply.github.com>
2021-12-20 11:05:26 +0300

    Update Fetchapi.js

--- utils/Fetchapi.js
+++ utils/Fetchapi.js
@@ -7,14 +7,9 @@ export const baseurl= 'https://bayut.p.rapidapi.com'
 export const Fetchapi= async (url)=>{
     const {data}= await axios.get((url),{
         headers: {
-            'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': 'd1ce00edd0msh4e691be2ceb7985p1a4120jsn7d158c6ecf1d'
+            
           }
     })
 
     return data;
 }
-/* headers: {
-    'x-rapidapi-host': 'bayut.p.rapidapi.com',
-    'x-rapidapi-key': 'd1ce00edd0msh4e691be2ceb7985p1a4120jsn7d158c6ecf1d'
-  } */
